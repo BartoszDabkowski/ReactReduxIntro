6446eccdcdf213f8cc3d7fa11dc81ca278194f88
Bartosz Dabkowski <BartoszDabkowski@Bartoszs-MacBook-Pro.local>
2016-06-26 23:20:38 -0700

    added reducers.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'JavaScript: The Good Parts' },
-        { title: 'Harry Potter' },
-        { title: 'Finding Nemo' },
-        { title: 'King Kong' },
+        { title: 'JavaScript: The Good Parts', pages: 101 },
+        { title: 'Harry Potter', pages: 34  },
+        { title: 'Finding Nemo', pages: 55  },
+        { title: 'King Kong', pages: 56  },
     ];
 }
